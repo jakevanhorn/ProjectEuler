3bbdd5bdbd9d0d695f1bdd44919aa3d9e01fcc22
jakevanhorn <jake.vanhorn13@gmail.com>
2016-11-27 21:30:40 -0800

    Changed Problem 3

--- Problem 3.cpp
+++ Problem 3.cpp
@@ -5,26 +5,26 @@ using namespace std;
 
 int main()
 {
-    long int number1 = 13195;
+	long int number1 = 600851475143;
     bool prime = false;
-    for (long int a = (number1 - 1); a > 0; a--)
+    for (long int a = (number1 - 1); a > 1; a--)
     {
         if (number1 % a == 0)
         {
-            for (long int b = a; b >= 1; b--)
+            for (long int b = (a-1); b >= 1; b--)
             {
                 if (b == 1)
                 {
                     prime = true;
                     break;
                 }
-                if (b % a == 0)
+                if (a % b == 0)
                 {
                     break;
                 }
-
             }
         }
+
         if (prime == true)
         {
             return a;
